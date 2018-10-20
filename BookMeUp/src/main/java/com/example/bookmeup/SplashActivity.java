b56379b13ab8d096682e1cdc6a154e81d34863df
Abiodun Ojo <abiodunojo.ca@gmail.com>
2018-10-20 19:43:27 -0400

    Splash Screen created.

--- BookMeUp/src/main/java/com/example/bookmeup/SplashActivity.java
+++ BookMeUp/src/main/java/com/example/bookmeup/SplashActivity.java
@@ -1,18 +1,16 @@
 package com.example.bookmeup;
 
 import android.content.Intent;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Intent intent = new Intent(getApplicationContext(),LoginActivity.class);
+        Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
         startActivity(intent);
         finish();
-       // setContentView(R.layout.activity_splash);
     }
 }
