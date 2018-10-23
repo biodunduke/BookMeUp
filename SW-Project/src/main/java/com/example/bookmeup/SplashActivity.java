dd96137a98f5d0797db1fa1848979271b5656940
Elias <n01114446@humbermail.ca>
2018-10-23 13:51:34 -0400

    Design modifications and small texts alterations
    1. 3 new icon packages added
    2. Registration paged modified with more info to sign up
    3. Login Page: Replaced the "optional" from the password field with ( minimum 8 characters )
    4.  Profile Page: Added new Profile generic image, modified some text fields

--- SW-Project/src/main/java/com/example/bookmeup/SplashActivity.java
+++ SW-Project/src/main/java/com/example/bookmeup/SplashActivity.java
@@ -1,15 +1,19 @@
 package com.example.bookmeup;
 
 import android.content.Intent;
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
 
 public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+        setContentView(R.layout.activity_splash);
+
+        Intent intent = new Intent(getApplicationContext(),
+                LoginActivity.class);
         startActivity(intent);
         finish();
     }
